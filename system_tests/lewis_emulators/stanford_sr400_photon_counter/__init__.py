8c5b11fa7e996413a6103eff88165b79c3d73486
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-06-06 14:37:30 +0100

    Add Lewis emulator

---
+++ system_tests/lewis_emulators/stanford_sr400_photon_counter/__init__.py
@@ -0,0 +1,5 @@
+from .device import SimulatedStanfordSr400PhotonCounter
+from ..lewis_versions import LEWIS_LATEST
+
+framework_version = LEWIS_LATEST
+__all__ = ['SimulatedStanfordSr400PhotonCounter']
---
+++ system_tests/lewis_emulators/stanford_sr400_photon_counter/interfaces/__init__.py
@@ -0,0 +1,3 @@
+from .stream_interface import StanfordSr400PhotonCounterStreamInterface
+
+__all__ = ['StanfordSr400PhotonCounterStreamInterface']
