f377cfc79c0014980413e429f0d536815fdb28b1
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-06-07 12:23:43 +0100

    Add basic tests

--- system_tests/tests/tests.py
+++ system_tests/tests/tests.py
@@ -5,6 +5,7 @@ from utils.ioc_launcher import get_default_ioc_dir
 from utils.test_modes import TestModes
 from utils.testing import get_running_lewis_and_ioc, skip_if_recsim
 
+import time
 
 DEVICE_PREFIX = "SR400_01"
 
@@ -19,7 +20,7 @@ IOCS = [
 ]
 
 
-TEST_MODES = [TestModes.RECSIM, TestModes.DEVSIM]
+TEST_MODES = [ TestModes.DEVSIM]
 
 
 class StanfordSr400PhotonCounterTests(unittest.TestCase):
@@ -30,5 +31,66 @@ class StanfordSr400PhotonCounterTests(unittest.TestCase):
         self._lewis, self._ioc = get_running_lewis_and_ioc("stanford_sr400_photon_counter", DEVICE_PREFIX)
         self.ca = ChannelAccess(device_prefix=DEVICE_PREFIX)
 
-    def test_that_fails(self):
-        self.fail("You haven't implemented any tests!")
+        self._simulate_restart_button_press()
+
+    def _simulate_start_button_press(self):
+        self.ca.set_pv_value("START_COUNTING", 1)
+
+    def _simulate_stop_button_press(self):
+        self.ca.set_pv_value("STOP_COUNTING", 1)
+
+    def _simulate_restart_button_press(self):
+        self.ca.set_pv_value("RESET_COUNTERS", 1)
+    
+    def test_counters_start(self):
+        self.ca.assert_that_pv_is("COUNTER:A:RBV", 0, timeout=2)
+        self.ca.assert_that_pv_is("COUNTER:B:RBV", 0, timeout=2)
+
+        self._simulate_start_button_press()
+
+        time.sleep(1)
+
+        self.ca.assert_that_pv_is_not("COUNTER:A:RBV", 0, timeout=2)
+        self.ca.assert_that_pv_is_not("COUNTER:B:RBV", 0, timeout=2)
+    
+    def test_counters_stop(self):
+        self._simulate_start_button_press()  # The fact that this works has been tested above
+
+        time.sleep(1)
+        
+        self._simulate_stop_button_press()
+
+        a_val = self.ca.get_pv_value("COUNTER:A:RBV")
+        b_val = self.ca.get_pv_value("COUNTER:B:RBV")
+
+        time.sleep(1)
+
+        self.ca.assert_that_pv_is("COUNTER:A:RBV", a_val)
+        self.ca.assert_that_pv_is("COUNTER:B:RBV", b_val)
+
+    def test_counters_restart(self):
+        self._simulate_start_button_press()  # The fact that this works has been tested above
+
+        time.sleep(1)
+        
+        self._simulate_restart_button_press()  # The fact that this works has been tested above
+
+        self.ca.assert_that_pv_is("COUNTER:A:RBV", 0)
+        self.ca.assert_that_pv_is("COUNTER:B:RBV", 0)
+
+    def test_status_changes(self):
+        self._simulate_start_button_press()  # The fact that this works has been tested above
+
+        time.sleep(1)
+        
+        status = self.ca.get_pv_value("STATUS")
+
+        time.sleep(1)
+
+        self.ca.assert_that_pv_is_not("STATUS", status)
+        status = self.ca.get_pv_value("STATUS")
+
+        time.sleep(1)
+
+        self.ca.assert_that_pv_is_not("STATUS", status)
+
